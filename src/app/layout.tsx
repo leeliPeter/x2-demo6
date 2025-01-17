cd7cf2f38ccc403fab017b04fa48237a078e9b44
Lei Ieong Tam <leeli.petertam@gmail.com>
2025-01-18 00:50:13 +0800

    feat: add nodedata function to rootlayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,31 @@
 import "./globals.css";
 import { Providers } from "@/redux/provider";
 import { getNavData } from "@/api/navData";
+import { getNodeData } from "@/api/nodeData";
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // Fetch nav data at the root level
-  const navData = await getNavData();
+  // Fetch nav data and node data at the root level
+  const [navData] = await getNavData();
+
+  // Get the first graph's node data
+  const firstGraph = navData.graph[0];
+  const nodeData = firstGraph?.graph_id
+    ? await getNodeData(firstGraph.graph_id)
+    : {
+        nodes: [],
+        links: [],
+      };
 
   return (
     <html lang="en">
       <body>
-        <Providers navData={navData[0]}>{children}</Providers>
+        <Providers navData={navData} nodeData={nodeData}>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -4,20 +4,26 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { createContext } from "react";
 import { NavData } from "@/api/navData";
+import { NodeData } from "@/api/nodeData";
 
 export const NavDataContext = createContext<NavData | null>(null);
+export const NodeDataContext = createContext<NodeData | null>(null);
 
 export function Providers({
   children,
   navData,
+  nodeData,
 }: {
   children: React.ReactNode;
   navData: NavData;
+  nodeData: NodeData;
 }) {
   return (
     <Provider store={store}>
       <NavDataContext.Provider value={navData}>
-        {children}
+        <NodeDataContext.Provider value={nodeData}>
+          {children}
+        </NodeDataContext.Provider>
       </NavDataContext.Provider>
     </Provider>
   );
